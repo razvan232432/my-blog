88392d088b855fba998946ba84a34bce38744670
razvan232432 <marius3837@proton.me>
2023-07-29 11:28:12 -0700

    Update LatestPosts.js

---
+++ src/components/LatestPosts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LatestPosts = () => {
+  return (
+    <div className='latest-posts'>
+        <h2>Latest Posts</h2>
+
+    </div>
+  )
+}
+
+export default LatestPosts
