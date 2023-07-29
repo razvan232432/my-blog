5f37449935511ae95fd9abfe57f4c660cb191cc9
razvan232432 <marius3837@proton.me>
2023-07-29 11:28:57 -0700

    Update Navigation.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Components.css'
+const Navigation = () => {
+  return (
+    <div>
+        <nav className='nav-link'>
+            <h2 className='title'>Daily dose of technology</h2>
+            <div className='links'>
+                <a href='1'>Home</a>
+                <a href='1'>Posts</a>
+                <a href='1'>Contact Me</a>
+                <a href='1'>Support</a>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navigation
