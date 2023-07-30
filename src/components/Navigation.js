753af9204c598cf16dac9dcfc3c12e780ca7cde8
razvan232432 <marius3837@proton.me>
2023-07-30 11:11:04 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,10 @@ const Navigation = () => {
         <nav className='nav-link'>
             <h2 className='title'>Daily dose of technology</h2>
             <div className='links'>
-                <a href='1'>Home</a>
-                <a href='1'>Posts</a>
-                <a href='1'>Contact Me</a>
-                <a href='1'>Support</a>
+                <a href='/src/App.js' target='_blank'>Home</a>
+                <a href='/src/pages/Posts.js' target='_blank'>Posts</a>
+                <a href='/src/pages/SignInLogin.js' target='_blank'>Sign In/Login</a>
+                <a href='/src/pages/Support.js' target='_blank'>Support</a>
             </div>
         </nav>
     </div>
