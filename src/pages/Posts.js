2ef9767223790c4c967582eb79d088e458d6c749
razvan232432 <marius3837@proton.me>
2023-07-30 11:20:20 -0700

    Update Posts.js

---
+++ src/pages/Posts.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+import Cards from '../components/Cards'
+
+const Posts = () => {
+  return (
+    <div>
+        <Navigation />
+        <div className='card-position'> 
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C7095-7A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        /> 
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        </div>
+    </div>
+  )
+}
+
+export default Posts
