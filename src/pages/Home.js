8fc5b0e42ac20b6d150dbb7d543f80ccc64c6aa7
razvan232432 <marius3837@proton.me>
2023-07-30 11:19:52 -0700

    Update Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+import LatestPosts from '../components/LatestPosts'
+
+function Home() {
+  return (
+    <div>
+        <Navigation />
+        <LatestPosts />
+    </div>
+  )
+}
+
+export default Home
