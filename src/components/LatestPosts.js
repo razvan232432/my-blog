a486af0091d8b6feaf827acac060c9c582f0588c
razvan232432 <marius3837@proton.me>
2023-07-30 10:18:23 -0700

    Update LatestPosts.js

--- src/components/LatestPosts.js
+++ src/components/LatestPosts.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Cards from './Cards'
+import './Components.css'
 
 const LatestPosts = () => {
   return (
     <div className='latest-posts'>
         <h2>Latest Posts</h2>
-
+        <div className='card-position'>
+        <Cards 
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713'
+        />
+        <Cards 
+             img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+             title='ROG Strix G17 (2023) G713'
+        /> 
+        <Cards
+            img='https://dlcdnwebimgs.asus.com/files/media/5C70957A-1479-4727-A0D0-1A860746F2F4/v2/img/slider6.jpg'
+            title='ROG Strix G17 (2023) G713' 
+        />
+        </div>
     </div>
   )
 }
