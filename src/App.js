93c9a6d16a2ca0b905de97341f0a444b78d8ec33
razvan232432 <marius3837@proton.me>
2023-08-01 00:32:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import SignInLogin from './pages/SignInLogin';
 import Support from './pages/Support';
 import Home from './pages/Home';
 function App() {
-  return (
-    
-   
+return  (    
     <div className="App">
       <Router>
-            <Home />
             <Routes>
-                <Route exact path='/' element={<Home />} />
+                <Route exact path='./'  element={<Home />} />
                 <Route path='/src/pages/Posts.js'element={<Posts />} />
                 <Route path='/src/pages/SignInLogin.js' element={<SignInLogin />} />
                 <Route path='/src/pages/Support.js' element={<Support />} />
             </Routes>
         </Router>
     </div>
-  );
+);
 }
 
 export default App;
